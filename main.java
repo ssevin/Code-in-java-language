b89298d6fc40f8c86000109b1c3be508b889a3bd
Sevin Nasiri <sevinnasiri85@gmail.com>
2025-02-28 22:13:04 +0330

    avg 2 numbers.

--- main.java
+++ main.java
@@ -7,5 +7,14 @@ public class main {
         a = scanner.nextInt();
         b = scanner.nextInt();
         System.out.println(a+b);
+
+        float x , y , z ;
+        System.out.println("Enter 3 numbers:");
+        x = scanner.nextFloat();
+        y = scanner.nextFloat();
+        z = scanner.nextFloat();
+        float result = (x + y + z)/3 ;
+        System.out.println("Avrage is:");
+        System.out.printf("%.2f" , result);
     }
 }
