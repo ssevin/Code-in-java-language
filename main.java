aa9787d29d9fbb191c6cb0ad541dc66b0c036461
Sevin Nasiri <sevinnasiri85@gmail.com>
2025-02-28 22:06:29 +0330

    sub 2 numbers(integer).

---
+++ main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args) {
+        int a , b ;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 2 numbers:");
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        System.out.println(a+b);
+    }
+}
